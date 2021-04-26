3ab92446d538df22c20c7b5bf8dc6acf76b6677e
Ali Imran <cwaliimran@gmail.com>
2021-04-26 11:56:42 +0500

    getBoards Request updated

--- AndroidApp/app/src/main/java/com/bingoplayer/app/utils/ApiInterface.java
+++ AndroidApp/app/src/main/java/com/bingoplayer/app/utils/ApiInterface.java
@@ -12,19 +12,27 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
 
+//    @POST("ManagePlayer")
+//    @FormUrlEncoded
+//    Call<ModelBoard> getBoards(
+//            @Field("game-id") String gameid,
+//            @Field("session-id") String sessionid,
+//            @Field("player-id") String playerid,
+//            @Field("op") String op,
+//            @Field("ver") String ver
+//    );
     @POST("ManagePlayer")
-    @FormUrlEncoded
     Call<ModelBoard> getBoards(
-            @Field("game-id") String gameid,
-            @Field("session-id") String sessionid,
-            @Field("player-id") String playerid,
-            @Field("op") String op,
-            @Field("ver") String ver
+            @Query("game-id") String gameid,
+            @Query("session-id") String sessionid,
+            @Query("player-id") String playerid,
+            @Query("op") String op,
+            @Query("ver") String ver
     );
-
 
     @POST("BingoAnswer/")
     @FormUrlEncoded
